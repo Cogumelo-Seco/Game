2c9bc35723cb34a32402e81dc4499a7bc1359a79
Wellington <wfrwellington13@gmail.com>
2021-07-02 00:14:46 -0300

    Mudança na cor errada

--- public/Render-Screen.js
+++ public/Render-Screen.js
@@ -41,7 +41,7 @@ export default function renderScreen(canvas, game, pingDisplay, requestAnimation
                     ctx.fillRect(x, y, 1, 1)
                 }
             }
-            ctx.fillStyle = '#dd6565';
+            ctx.fillStyle = '#f03434';
             ctx.globalAlpha = 1
             ctx.fillRect(0, (Number.parseInt(canvas.height/2))-myPlayer.y, canvas.width*2, -canvas.height*2);
             ctx.fillRect(0, (Number.parseInt(canvas.height/2)+game.state.screen.height)-myPlayer.y, canvas.width*2, canvas.height*2);
