73dd0e7837d4e2aee3a05d15c05e94cc12a1879d
Wellington <wfrwellington13@gmail.com>
2021-11-24 20:57:36 -0300

    Categoria 'todos' e sistema de pesquisa em comandos

--- public/js/Game/RenderScreen/RenderInformationTexts.js
+++ public/js/Game/RenderScreen/RenderInformationTexts.js
@@ -1,6 +1,6 @@
 module.exports = (canvas, game, Listener, scoreArr) => {
     let player = game.state.players[game.state.myID]
-    if (player.dead) player = game.state.players[game.state.observedPlayerId]
+    //if (player.dead) player = game.state.players[game.state.observedPlayerId]
 
     const playerScore = document.getElementById('playerScore');
     const fpsDisplay = document.getElementById('fpsDisplay');
